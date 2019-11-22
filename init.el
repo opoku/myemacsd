97e61bdb971223daf935a3d318077791b8d24d1a
Osei Poku <opoku@users.noreply.github.com>
2019-11-22 10:35:11 -0500

    COmment

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
   (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo -n $PATH'")))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
-
+;; Testing this
 
 
 (when window-system
